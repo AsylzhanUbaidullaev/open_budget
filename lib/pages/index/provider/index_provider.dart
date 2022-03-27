a2c32fee04598378b751f91de8b535d679f93e52
Asylzhan <asylzhan1809200@gmail.com>
2022-03-27 19:18:37 +0600

    first

---
+++ lib/pages/index/provider/index_provider.dart
@@ -0,0 +1,9 @@
+import 'package:open_budget/base/base_bloc.dart';
+
+class IndexProvider extends BaseBloc {
+  init(context) {
+    setLoading(true);
+
+    setLoading(false);
+  }
+}
---
+++ lib/pages/login/provider/registration_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:open_budget/base/base_bloc.dart';
+import 'package:open_budget/pages/login/ui/login.dart';
+
+class RegistrationProvider extends BaseBloc {
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController surnameController = TextEditingController();
+  bool isButtonEnabled = false;
+  String? phoneError;
+
+  toLogin(context) {
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const Login()),
+        (route) => false);
+  }
+
+  checkPhoneNumber(String value) {
+    if (value.length == 12) {
+      isButtonEnabled = true;
+      notifyListeners();
+    } else {
+      isButtonEnabled = false;
+      notifyListeners();
+    }
+  }
+}
